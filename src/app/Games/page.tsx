eafc815f5efcd9d43d8766ec22677b3ceea724c4
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-09-06 16:30:04 +0300

    added game page

---
+++ src/app/Games/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      {/* write all IFrame of your games then i will do the rest ("-") */}
+    </main>
+  );
+}
