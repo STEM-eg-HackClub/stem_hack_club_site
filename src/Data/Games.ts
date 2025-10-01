2d4634fd1c01e542c5c7559f7983f66a2e3cbf32
محمد عبدالعزيز عبدالعزيز المليجي <136956569+DsMans0021@users.noreply.github.com>
2025-10-01 09:53:19 +0300

    updates broooo

--- src/Data/Games.ts
+++ src/Data/Games.ts
@@ -5,4 +5,40 @@ export const Games = [
     Game: "https://itch.io/embed-upload/13451657?color=333333",
     ItchIo: "https://itskareem.itch.io/maya",
   },
+  {
+    title: "Prince DsMans0021",
+    author: "daniel-geo",
+    Game: "https://itch.io/embed-upload/14653017?color=333333",
+    ItchIo: "https://daniel-geo.itch.io/prince-dsmans0021",
+  },
+  {
+    title: "Pong game",
+    author: "daniel-geo",
+    Game: "https://itch.io/embed-upload/14847101?color=333333",
+    ItchIo: "https://daniel-geo.itch.io/pong",
+  },
+  {
+    title: "The Lego Advinture",
+    author: "daniel-geo",
+    Game: "https://itch.io/embed-upload/14846426?color=333333",
+    ItchIo: "https://daniel-geo.itch.io/lego",
+  },
+  {
+    title: "Tiny Quest",
+    author: "daniel-geo",
+    Game: "https://itch.io/embed-upload/14918388?color=333333",
+    ItchIo: "https://daniel-geo.itch.io/tiny-quest",
+  },
+  {
+    title: "Flappy bird",
+    author: "alyalgendy",
+    Game: "https://itch.io/embed-upload/14198405?color=333333",
+    ItchIo: "https://alyalgendy.itch.io/flappy-bird",
+  },
+  {
+    title: "Try to Die",
+    author: "3maar",
+    Game: "https://itch.io/embed-upload/14931189?color=333333",
+    ItchIo: "https://3maar.itch.io/final-project",
+  },
 ];
